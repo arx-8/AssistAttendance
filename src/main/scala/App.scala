aaca7b1207e4da8c452777e879f9b12f324a50db
swan0 <swans.randomness@gmail.com>
2016-11-01 23:34:08 +0900

    とりまserver

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,5 +1,54 @@
+import java.io.{ByteArrayOutputStream, InputStream}
+import java.net.InetSocketAddress
+import java.util.concurrent.Executors
+
+import com.sun.net.httpserver.{HttpExchange, HttpHandler, HttpServer}
+
 object App {
+
   def main(args: Array[String]): Unit = {
-    println("Hello")
+    val server = HttpServer.create(new InetSocketAddress("localhost", 5000), 5000)
+    val threadPool = Executors.newFixedThreadPool(1)
+    server.setExecutor(threadPool)
+
+    // redirect invalid url
+    server.createContext("/", new HttpHandler {
+      override def handle(exchange: HttpExchange): Unit = {
+        exchange.getResponseHeaders.add("Location", "/index/")
+        exchange.sendResponseHeaders(301, 0)
+        exchange.getResponseBody.close()
+      }
+    })
+    // index
+    val url = getClass.getClassLoader.getResource("index.html")
+    server.createContext("/index/", new HttpHandler {
+      override def handle(exchange: HttpExchange): Unit = {
+        val buf = convertByteArray(url.openStream())
+        exchange.sendResponseHeaders(200, buf.length)
+        exchange.getResponseBody.write(buf)
+        exchange.getResponseBody.close()
+      }
+    })
+
+    server.start()
+    println("running...")
   }
+
+
+  def convertByteArray(is: InputStream): Array[Byte] = {
+    val os = new ByteArrayOutputStream()
+    // TODO byte固定長で大丈夫なの？
+    val buffer = new Array[Byte](1024)
+    while (true) {
+      val len = is.read(buffer)
+      if (0 <= len) {
+        os.write(buffer, 0, len)
+      } else {
+        return os.toByteArray
+      }
+    }
+    // TODO unreachable
+    null
+  }
+
 }
