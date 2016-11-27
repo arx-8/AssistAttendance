e97e56a57b3cd8b348a17b21e2e40d722e094a17
swan0 <swans.randomness@gmail.com>
2016-11-27 23:10:25 +0900

    slf4jのログ抑制

--- src/main/scala/com/example/controller/SlackController.scala
+++ src/main/scala/com/example/controller/SlackController.scala
@@ -1,10 +1,16 @@
 package com.example.controller
 
 import akka.actor.ActorSystem
+import ch.qos.logback.classic.LoggerContext
 import com.example.Settings
+import org.slf4j.LoggerFactory
 import slack.rtm.SlackRtmClient
 
+import scala.language.postfixOps
+
 object SlackController {
+  suppressLogging()
+
   implicit val system = ActorSystem("slack")
   implicit val ec = system.dispatcher
 
@@ -17,5 +23,10 @@ object SlackController {
 
   def sendMessage(text: String) = {
     client.sendMessage(channelId, text)
+  }
+
+  private def suppressLogging() = {
+    val logger = LoggerFactory.getILoggerFactory().asInstanceOf[LoggerContext]
+    logger.stop()
   }
 }
