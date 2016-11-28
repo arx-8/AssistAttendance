935202770261a527b962a28ff2b7aa270e28d640
swan0 <swans.randomness@gmail.com>
2016-11-28 23:32:24 +0900

    非公開チャンネルに投稿できない問題の修正

--- src/main/scala/com/example/controller/SlackController.scala
+++ src/main/scala/com/example/controller/SlackController.scala
@@ -23,13 +23,7 @@ object SlackController {
   // これがないと、初回のsendMessageが失敗する？
   Thread.sleep(5000L)
 
-  private val channelId = {
-    val temp = client.state.getChannelIdForName(Settings.slack.postChName)
-    temp match {
-      case Some(chId) => chId
-      case None => throw new NoSuchElementException("Slackのチャンネル名「" + Settings.slack.postChName + "」が見つかりませんでした。")
-    }
-  }
+  private val channelId = getChannelIdBy(client, Settings.slack.postChName)
 
   def sendMessage(text: String) = {
     client.sendMessage(channelId, text)
@@ -38,5 +32,23 @@ object SlackController {
   private def suppressLogging() = {
     val logger = LoggerFactory.getILoggerFactory().asInstanceOf[LoggerContext]
     logger.stop()
+  }
+
+  /**
+    * チャンネルIDを返す。
+    * 公開・非公開チャンネル、両対応。
+    *
+    * @param channelName
+    * @return
+    */
+  private def getChannelIdBy(client: SlackRtmClient, channelName: String): String = {
+    // public ch
+    client.state.getChannelIdForName(channelName).getOrElse {
+      // private ch
+      client.state.groups.find(_.name == channelName).map(_.id) match {
+        case Some(chId) => chId
+        case None => throw new NoSuchElementException(s"Slackのチャンネル名「$channelName」が見つかりませんでした。")
+      }
+    }
   }
 }
